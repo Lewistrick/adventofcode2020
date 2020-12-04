01542ffdbb3933ddac0f406b9e4df2e5ec885101
Lewistrick <erickwilts@gmail.com>
2020-12-04 07:51:37 +0100

    Small changes in passport validation (04.2)

--- passport.py
+++ passport.py
@@ -2,10 +2,12 @@ import re
 from loguru import logger
 
 class Passport:
+    # required password fields
     reqfields = ("byr", "iyr", "eyr", "hgt",
                  "hcl", "ecl", "pid")
+    # optional password fields (not used yet)
     optfields = ("cid", )
-
+    # regexes for password fields
     field_rx = {
         "hgt": re.compile(r"^([0-9]{2,3})(cm|in)$"),
         "hcl": re.compile(r"^#[0-9a-f]{6}$"),
@@ -27,55 +29,49 @@ class Passport:
         return cls(**kwargs)
 
     def is_valid_day04_part1(self):
-        """Checks validity of a passport (day 4 part 1)"""
+        """Checks validity of a passport"""
         return all(hasattr(self, attr) for attr in self.reqfields)
 
     def is_valid_day04_part2(self):
+        """Checks validity of a passport"""
         if not self.is_valid_day04_part1():
-            logger.debug("Invalid: not all required fields present")
+            # logger.debug("Invalid: not all required fields present")
             return False
         # birth year
         if int(self.byr) not in range(1920, 2003):
-            logger.debug(f"Invalid birth year {self.byr}")
+            # logger.debug(f"Invalid birth year {self.byr}")
             return False
         # issue year
         if int(self.iyr) not in range(2010, 2021):
-            logger.debug(f"Invalid issue year {self.iyr}")
+            # logger.debug(f"Invalid issue year {self.iyr}")
             return False
         # expiration year
         if int(self.eyr) not in range(2020, 2031):
-            logger.debug(f"Invalid expiration year {self.eyr}")
+            # logger.debug(f"Invalid expiration year {self.eyr}")
             return False
         # height
         hgtmatch = self.field_rx["hgt"].match(self.hgt)
         if not hgtmatch:
-            logger.debug(f"Invalid height: {self.hgt}")
+            # logger.debug(f"Invalid height: {self.hgt}")
             return False
         value, unit = hgtmatch.groups()
         if unit == "cm" and int(value) not in range(150, 194):
-            logger.debug(f"Too short/tall: {self.hgt}")
+            # logger.debug(f"Too short/tall: {self.hgt}")
             return False
         elif unit == "in" and int(value) not in range(59, 77):
-            logger.debug(f"Too short/tall: {self.hgt}")
+            # logger.debug(f"Too short/tall: {self.hgt}")
             return False
         elif unit not in ("cm", "in"):
-            logger.debug(f"Invalid unit: {self.hgt}")
-            return False
-        # hair color
-        hclmatch = self.field_rx["hcl"].match(self.hcl)
-        if not hclmatch:
-            logger.debug(f"Invalid hair color: {self.hcl}")
-            return False
-        # eye color
-        eclmatch = self.field_rx["ecl"].match(self.ecl)
-        if not eclmatch:
-            logger.debug(f"Invalid eye color match: {self.ecl}")
-            return False
-        # passport ID
-        pidmatch = self.field_rx["pid"].match(self.pid)
-        if not pidmatch:
-            logger.debug(f"Invalid passport ID: {self.pid}")
+            # logger.debug(f"Invalid unit: {self.hgt}")
             return False
 
-        logger.success("Valid!")
+        # And finally, some simple matches
+        for field in ("hcl", "ecl", "pid"):
+            value = getattr(self, field)
+            fieldmatch = self.field_rx[field].match(value)
+            if not fieldmatch:
+                # logger.debug(f"Invalid {field}: {value}")
+                return False
+
+        # logger.success("Valid!")
         return True
