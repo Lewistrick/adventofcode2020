0fa79b94200bf21c8aed01e3de938d432aa02364
Lewistrick <erickwilts@gmail.com>
2020-12-05 15:21:47 +0100

    Seat ID optimization

--- day05.py
+++ day05.py
@@ -1,32 +1,41 @@
-# read boarding passes
-with open("day05.txt") as day05:
-    bpasses = day05.read()
+import timeit
 
-ids = set()
-part1 = 0
-let2bin = {"F": "0", "B": "1", "L": "0", "R": "1"}
-for bp in bpasses.split():
-    # read row as 0s and 1s
-    binrow = "".join(let2bin[let] for let in bp[:7])
-    # convert binary to decimal
-    rowno = int(binrow, 2)
+def calc_day05():
+    # read boarding passes
+    with open("day05.txt") as day05:
+        bpasses = day05.read()
 
-    binseat = "".join(let2bin[let] for let in bp[7:])
-    seatno = int(binseat, 2)
+    ids = set()
+    part1 = 0
 
-    # Calculate seatid. This could also be done
-    seatid = rowno * 8 + seatno
+    let2bin = {"F": "0", "B": "1", "L": "0", "R": "1"}
+    for bp in bpasses.split():
+        binbp = "".join(let2bin[let] for let in bp)
+        seatid = int(binbp, 2)
+        part1 = max(part1, seatid)
+        ids.add(seatid)
 
-    part1 = max(part1, seatid)
+    ids = sorted(ids)
+    for id1, id2 in zip(ids[:-1], ids[1:]):
+        if id2 - id1 > 1:
+            part2 = id1 + 1
+            break
 
-    ids.add(seatid)
+    return part1, part2
 
-print(part1)
 
-ids = sorted(ids)
-for id1, id2 in zip(ids[:-1], ids[1:]):
-    if id2 - id1 > 1:
-        part2 = id1 + 1
-        break
+if __name__ == "__main__":
+    part1, part2 = calc_day05()
 
-print(part2)
+    print(part1)
+    print(part2)
+
+    # timing command:
+    # python -m timeit -s "from day05 import calc_day05" "calc_day05()"
+
+    # baseline
+    # 100 loops, best of 5: 3.82 msec per loop
+
+    # after removing the split between row and seat
+    #   (because seat id is represented in binary)
+    # 100 loops, best of 5: 2.05 msec per loop
